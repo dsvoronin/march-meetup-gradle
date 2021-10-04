05f0b718e7e2414d8271478633598c5ee860dd24
Dmitriy Voronin <dvoronin@avito.ru>
2021-10-04 23:31:49 +0300

    rxjava example for dependencies lock

--- libraryModule1/src/main/java/com/libraryModule1/Activity0.java
+++ libraryModule1/src/main/java/com/libraryModule1/Activity0.java
@@ -1,15 +1,18 @@
 package com.libraryModule1;
 
 import android.widget.ImageView;
-import java.lang.Override;
+
+import io.reactivex.rxjava3.plugins.RxJavaPlugins;
 
 public class Activity0 extends android.app.Activity {
-  ImageView imageView0;
+    ImageView imageView0;
 
-  @Override
-  public void onCreate(android.os.Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    new libraryModule1packageKt0.Foo0().foo9();
-    setContentView(R.layout.librarymodule1activity_main0);
-  }
+    @Override
+    public void onCreate(android.os.Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new libraryModule1packageKt0.Foo0().foo9();
+        setContentView(R.layout.librarymodule1activity_main0);
+
+        RxJavaPlugins.lockdown();
+    }
 }
