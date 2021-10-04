e296a622c612be498ec082f7c9e148aab3af0cbc
Dmitriy Voronin <dvoronin@avito.ru>
2021-10-04 23:36:32 +0300

    rxjava example for dependencies lock

--- build-logic/src/main/kotlin/dependency-locking-convention.gradle.kts
+++ build-logic/src/main/kotlin/dependency-locking-convention.gradle.kts
@@ -9,15 +9,14 @@ tasks.register("resolveAndLockAll") {
         }
     }
 
-//    doLast {
-//        configurations.filter { it.isCanBeResolved }.forEach { it.resolve() }
-//    }
-
     /**
      * Can't use "resolve" method, probably because of some project misconfiguration,
      * seems like dependencies will do the same
      *
      * see https://docs.gradle.org/current/userguide/dependency_locking.html#lock_all_configurations_in_one_build_execution
      */
+    //    doLast {
+    //        configurations.filter { it.isCanBeResolved }.forEach { it.resolve() }
+    //    }
     dependsOn(tasks.named("dependencies"), tasks.named("buildEnvironment"))
 }
